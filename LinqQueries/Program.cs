1f3824fe06f3c390bc43672d9068d81c1297433c
hp <sabhrwalkanika1999@gmail.com>
2023-10-13 23:55:14 +0530

    Add project files.

---
+++ LinqQueries/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqQueries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {22,34,1,2,44,23,13,14,24,11 };
+
+            var a = from i in arr where i>20 orderby i select i;
+
+            foreach (var i in a)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("-------------------------------------------------------");
+
+            string[] s = {"Hello","Good Morning", "Good Night" , "GoodBye" };
+
+            var st = from greet in s where greet.Contains("Good") select greet;
+            foreach (string greet in st)
+            {
+                Console.WriteLine(greet);
+            }
+            Console.ReadLine();
+        }
+    }
+}
